b93dbc7abfac21f9d9e0c7e00832898bd7248008
Markus Ahrweiler <Mephiztopheles@users.noreply.github.com>
2018-09-28 10:32:48 +0200

    added another polygon

--- test/Polygon/index.js
+++ test/Polygon/index.js
@@ -16,3 +16,17 @@ polygon.style.lineWidth = 2;
 
 polygon.paint(ctx);
 console.log(polygon);
+
+
+
+canvas = document.getElementById("canvas-2");
+ctx = canvas.getContext("2d");
+
+var polygon2 = new Polygon(8, 0, new Point(canvas.width / 2, canvas.height / 2), 0);
+
+polygon2.setRadius((canvas.height - 10) / 2);
+
+polygon2.style.fillStyle = "green";
+polygon2.style.lineWidth = 2;
+
+polygon2.paint(ctx);
