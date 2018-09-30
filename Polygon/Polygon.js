3b85178a18cc20de963e2a4b93f24807f7d3a689
Markus Ahrweiler <markus.ahrweiler@web.de>
2018-09-30 17:53:05 +0200

    polygon improvements and dynamic repaint

--- Polygon/Polygon.js
+++ Polygon/Polygon.js
@@ -1,137 +1,261 @@
+const privates = new WeakMap();
+
 export class Point {
 
-    constructor(x, y) {
+    constructor( x, y ) {
 
         this.x = x;
         this.y = y;
     }
 }
 
-function value(a, b) {
+function value( a, b ) {
     return a == null ? b : a;
 }
 
 export class Side {
 
-    constructor(start, end) {
+    constructor( polygon, index, start, end ) {
 
+
+        this.polygon = polygon;
+
+        this.index = index;
         this.start = start;
         this.end = end;
+        this.style = new CanvasStyle;
     }
 
+    paintIndex( context ) {
+
+        if ( !this.style.paintIndex )
+            return;
+
+        const font = context.font;
+        const fillStyle = context.fillStyle;
+        context.font = "11px Arial";
+        context.fillStyle = "black";
+        let x = this.start.x;
+        let y = this.start.y;
+
+
+        x += ( this.end.x - this.start.x ) / 2;
+        if ( x < this.polygon.middle.x )
+            x -= 10;
+        else
+            x += 5;
+
+        y += ( this.end.y - this.start.y ) / 2;
+        if ( y < this.polygon.middle.y )
+            y -= 5;
+        else
+            y += 10;
+
+
+        context.fillText( this.index, x, y );
+        context.font = font;
+        context.fillStyle = fillStyle;
+    }
 }
 
 export class CanvasStyle {
 
-    constructor() {
-    }
-	
-	static defaults(){
-		
-		let style = new CanvasStyle();
-		
-		style.fillStyle = "transparent";
+    static defaults() {
+
+        let style = new CanvasStyle();
+
+        style.fillStyle = "transparent";
         style.strokeStyle = "#000000";
         style.shadowBlur = 0;
         style.shadowColor = "#000000";
-        style.shadowOffsetX = 0
-        style.shadowOffsetY = 0
+        style.shadowOffsetX = 0;
+        style.shadowOffsetY = 0;
         style.lineCap = "butt";
         style.lineJoin = "miter";
         style.lineWidth = 1;
         style.miterLimit = 10;
-		
-		return style;
-	}
+        style.paintIndex = false;
+
+        return style;
+    }
+}
+
+function getRadius( polygon ) {
+    return polygon.lengthOfSide / ( 2 * Math.sin( Math.PI / polygon.edgesCount ) );
+}
+
+function getCentralAngle( polygon ) {
+    return 2 * Math.PI * polygon.winding / polygon.edgesCount;
+}
+
+function getAngle( polygon ) {
+    return 180 - 360 / polygon.edgesCount;
 }
 
 export class Polygon {
 
-    constructor(edgesCount, lengthOfSide, middle, rotation = 0, winding = 1) {
-		
-		if(edgesCount <3)
-			throw new Error("Did you ever see a polygon with less than 3 sides?")
+    constructor( edgesCount, lengthOfSide, middle, rotation = 0, winding = 1 ) {
+
+        let style = CanvasStyle.defaults();
+        privates.set( this, {
+            lengthOfSide,
+            rotation,
+            winding,
+            middle,
+            style
+        } );
 
         this.edgesCount = edgesCount;
-        this.lengthOfSide = lengthOfSide;
-        this.rotation = rotation;
-        this.winding = winding;
-        this.middle = middle;
-        this.radius = this.lengthOfSide / (2 * Math.sin(Math.PI / this.edgesCount));
-        this.centralAngle = 2 * Math.PI * this.winding / this.edgesCount;
-        this.angle = 180 - 360 / this.edgesCount;
-        this.style = CanvasStyle.defaults();
 
-        this.regpoly();
+        this.calculateSides();
     }
 
-    setRadius(radius) {
-
-        this.radius = radius;
-        this.lengthOfSide = radius * (2 * Math.sin(Math.PI / this.edgesCount));
-        this.regpoly();
+    get edgesCount() {
+        return privates.get( this ).edgesCount;
     }
 
-    paint(context) {
+    set edgesCount( value ) {
 
-        if (this.style == null)
-            throw new Error("can't draw without style");
-		if(this.style.fillStyle && this.style.fillStyle != "transparent") {
-			console.log("fill");
-			
-			context.beginPath();
-			context.fillStyle = this.style.fillStyle;
-			
-			
-				context.moveTo(this.sides[0].start.x, this.sides[0].start.y);
-			
-			this.sides.forEach((side) => {
-				context.lineTo(side.end.x, side.end.y);
-			});
-			context.fill();
-			context.closePath();
-		}
+        if ( value < 3 )
+            throw new Error( "Did you ever see a polygon with less than 3 sides?" );
 
-        this.sides.forEach((side) => {
+        privates.get( this ).edgesCount = value;
+        privates.get( this ).centralAngle = getCentralAngle( this );
+        privates.get( this ).angle = getAngle( this );
+        this.calculateSides();
+        return value;
+    }
+
+    get radius() {
+        return privates.get( this ).radius;
+    }
+
+    set radius( value ) {
+
+        privates.get( this ).radius = value;
+        privates.get( this ).lengthOfSide = value * ( 2 * Math.sin( Math.PI / this.edgesCount ) );
+        this.calculateSides();
+
+        return value;
+    }
+
+    get lengthOfSide() {
+        return privates.get( this ).lengthOfSide;
+    }
+
+    set lengthOfSide( value ) {
+
+        privates.get( this ).lengthOfSide = value;
+        privates.get( this ).radius = getRadius( this );
+
+        return value;
+    }
+
+    get centralAngle() {
+        return privates.get( this ).centralAngle;
+    }
+
+    set centralAngle( value ) {
+
+        privates.get( this ).centralAngle = value;
+        this.calculateSides();
+        return value;
+    }
+
+    get rotation() {
+        return privates.get( this ).rotation;
+    }
+
+    set rotation( value ) {
+
+        privates.get( this ).rotation = value;
+        this.calculateSides();
+        return value;
+    }
+
+    get style() {
+        return privates.get( this ).style;
+    }
+
+    get middle() {
+        return privates.get( this ).middle;
+    }
+
+    set middle( value ) {
+        return privates.get( this ).middle = value;
+    }
+
+    get winding() {
+        return privates.get( this ).winding;
+    }
+
+    set winding( value ) {
+
+        privates.get( this ).winding = value;
+        this.centralAngle = getCentralAngle( this );
+        return value;
+    }
+
+    paint( context ) {
+
+        if ( this.style.fillStyle && this.style.fillStyle !== "transparent" ) {
 
             context.beginPath();
-			this.applyStyle(context, side.style || this.style);
-            context.moveTo(side.start.x, side.start.y);
-            context.lineTo(side.end.x, side.end.y);
-            context.stroke();
+            context.fillStyle = this.style.fillStyle;
+
+
+            context.moveTo( this.sides[ 0 ].start.x, this.sides[ 0 ].start.y );
+
+            this.sides.forEach( ( side ) => {
+                context.lineTo( side.end.x, side.end.y );
+            } );
+            context.fill();
             context.closePath();
-        });
+        }
+
+        this.sides.forEach( ( side, index ) => {
+
+            context.beginPath();
+            this.applyStyle( context, side.style || this.style );
+            context.moveTo( side.start.x, side.start.y );
+            context.lineTo( side.end.x, side.end.y );
+            context.stroke();
+
+            side.paintIndex( context );
+
+            context.closePath();
+        } );
     }
 
-    applyStyle(context, style) {
+    applyStyle( context, style ) {
 
-        context.strokeStyle = value(style.strokeStyle, this.style.strokeStyle);
-        context.shadowBlur = value(style.shadowBlur, this.style.shadowBlur);
-        context.shadowColor = value(style.shadowColor, this.style.shadowColor);
-        context.shadowOffsetX = value(style.shadowOffsetX, this.style.shadowOffsetX);
-        context.shadowOffsetY = value(style.shadowOffsetY, this.style.shadowOffsetY);
-        context.lineCap = value(style.lineCap, this.style.lineCap);
-        context.lineJoin = value(style.lineJoin, this.style.lineJoin);
-        context.lineWidth = value(style.lineWidth, this.style.lineWidth);
-        context.miterLimit = value(style.miterLimit, this.style.miterLimit);
+        context.strokeStyle = value( style.strokeStyle, this.style.strokeStyle );
+        context.shadowBlur = value( style.shadowBlur, this.style.shadowBlur );
+        context.shadowColor = value( style.shadowColor, this.style.shadowColor );
+        context.shadowOffsetX = value( style.shadowOffsetX, this.style.shadowOffsetX );
+        context.shadowOffsetY = value( style.shadowOffsetY, this.style.shadowOffsetY );
+        context.lineCap = value( style.lineCap, this.style.lineCap );
+        context.lineJoin = value( style.lineJoin, this.style.lineJoin );
+        context.lineWidth = value( style.lineWidth, this.style.lineWidth );
+        context.miterLimit = value( style.miterLimit, this.style.miterLimit );
     }
 
-    regpoly() {
+    calculateSides() {
 
         this.edges = [];
         this.sides = [];
-        for (var k = 1; k <= this.edgesCount; k++) {
+        for ( let k = 1; k <= this.edgesCount; k++ ) {
 
-            var x1 = this.radius * Math.cos(k * this.centralAngle + this.rotation) + this.middle.x;
-            var y1 = this.radius * Math.sin(k * this.centralAngle + this.rotation) + this.middle.y;
+            let x1 = this.radius * Math.cos( k * this.centralAngle + this.rotation ) + this.middle.x;
+            let y1 = this.radius * Math.sin( k * this.centralAngle + this.rotation ) + this.middle.y;
 
 
-            var x2 = this.radius * Math.cos((k + 1) * this.centralAngle + this.rotation) + this.middle.x;
-            var y2 = this.radius * Math.sin((k + 1) * this.centralAngle + this.rotation) + this.middle.y;
+            let x2 = this.radius * Math.cos( ( k + 1 ) * this.centralAngle + this.rotation ) + this.middle.x;
+            let y2 = this.radius * Math.sin( ( k + 1 ) * this.centralAngle + this.rotation ) + this.middle.y;
 
-            var side = new Side(new Point(x1, y1), new Point(x2, y2));
-            this.sides.push(side);
-            this.edges.push(side.start);
+            let side = new Side( this, k, new Point( x1, y1 ), new Point( x2, y2 ) );
+            this.sides.push( side );
+            this.edges.push( side.start );
         }
     }
 }
--- test/Polygon/index.js
+++ test/Polygon/index.js
@@ -1,32 +1,97 @@
-import {Polygon,Point,CanvasStyle} from "../../Polygon/Polygon.js";
+import { CanvasStyle, Point, Polygon } from "../../Polygon/Polygon.js";
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+let canvas = document.getElementById( "canvas" );
+let context = canvas.getContext( "2d" );
 
-var polygon = new Polygon(3, 0, new Point(canvas.width / 2, canvas.height / 2), 0);
+let polygon = new Polygon( 3, 0, new Point( canvas.width / 2, canvas.height / 2 ), 0 );
 
-polygon.setRadius((canvas.height - 10) / 2);
+polygon.radius = ( ( canvas.height - 20 ) / 2 );
 
 
-polygon.sides[0].style = new CanvasStyle();
-polygon.sides[0].style.strokeStyle = "red";
+polygon.sides[ 0 ].style = new CanvasStyle();
+polygon.sides[ 0 ].style.strokeStyle = "red";
 
 polygon.style.fillStyle = "red";
 polygon.style.lineWidth = 2;
 
-polygon.paint(ctx);
-console.log(polygon);
+const module = angular.module( "app", [] );
 
 
+function drawGrid( ctx ) {
 
-canvas = document.getElementById("canvas-2");
-ctx = canvas.getContext("2d");
+    const data = "<svg width=\"100%\" height=\"100%\" xmlns=\"http://www.w3.org/2000/svg\"> \
+        <defs> \
+            <pattern id=\"smallGrid\" width=\"8\" height=\"8\" patternUnits=\"userSpaceOnUse\"> \
+                <path d=\"M 8 0 L 0 0 0 8\" fill=\"none\" stroke=\"gray\" stroke-width=\"0.5\" /> \
+            </pattern> \
+            <pattern id=\"grid\" width=\"80\" height=\"80\" patternUnits=\"userSpaceOnUse\"> \
+                <rect width=\"80\" height=\"80\" fill=\"url(#smallGrid)\" /> \
+                <path d=\"M 80 0 L 0 0 0 80\" fill=\"none\" stroke=\"gray\" stroke-width=\"1\" /> \
+            </pattern> \
+        </defs> \
+        <rect width=\"100%\" height=\"100%\" fill=\"url(#smallGrid)\" /> \
+    </svg>";
 
-var polygon2 = new Polygon(8, 0, new Point(canvas.width / 2, canvas.height / 2), 0);
+    const DOMURL = window.URL || window.webkitURL || window;
 
-polygon2.setRadius((canvas.height - 10) / 2);
+    const img = new Image();
+    const svg = new Blob( [ data ], { type: "image/svg+xml;charset=utf-8" } );
+    const url = DOMURL.createObjectURL( svg );
 
-polygon2.style.fillStyle = "green";
-polygon2.style.lineWidth = 2;
+    img.onload = function () {
 
-polygon2.paint(ctx);
+        ctx.drawImage( img, 0, 0 );
+        DOMURL.revokeObjectURL( url );
+    };
+    img.src = url;
+}
+
+
+module.controller( "AppController", [ "$scope", function ( $scope ) {
+
+    drawGrid( context );
+    polygon.paint( context );
+    $scope.polygon = polygon;
+    $scope.hidden = [];
+    $scope.hideAll = false;
+    $scope.repaint = function () {
+
+        if ( $scope.hideAll )
+            hideAll();
+
+        if ( $scope.paintAllIndices )
+            paintAllIndices( true );
+
+        context.clearRect( 0, 0, canvas.width, canvas.height );
+        drawGrid( context );
+        polygon.paint( context );
+    };
+
+    function hideAll() {
+
+        $scope.hidden.length = 0;
+
+        if ( $scope.hideAll )
+            for ( let i = 0; i < $scope.polygon.sides.length; i++ )
+                $scope.hidden[ i ] = true;
+    }
+
+    $scope.$watch( "hideAll", hideAll );
+
+    function paintAllIndices( value ) {
+
+        $scope.polygon.sides.forEach( side => {
+            side.style.paintIndex = value;
+        } );
+    }
+
+    $scope.$watch( "paintAllIndices", function ( value, old ) {
+
+        if ( value !== old ) {
+            paintAllIndices( value );
+
+            $scope.repaint();
+        }
+    } );
+} ] );
+angular.bootstrap( document.getElementById( "center" ), [ "app" ] );
